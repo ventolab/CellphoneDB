cd267436c58c0fef1693f9de7ffafd02e33cf724
Robert Petryszak <info@datasome.co.uk>
2022-12-27 17:41:19 +0000

    Documentation via sphinx and associated changes

--- cellphonedb/src/exceptions/ParseCountsException.py
+++
@@ -1,5 +0,0 @@
-class ParseCountsException(Exception):
-    def __init__(self, description: str = None, hint: str = None):
-        super(ParseCountsException, self).__init__('Invalid Counts data')
-        self.description = description
-        self.hint = hint
