70ba7ae84ab1ccbd5f8fbec1efc6b18460af6edb
Robert Petryszak <info@datasome.co.uk>
2023-03-10 18:03:07 +0000

    Amended file, ready for package publication

--- setup.py
+++ setup.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages
+from cellphonedb import version
 
-import setuptools
+with open("README.md", "rt", encoding="utf-8") as fh:
+    long_description_readme_md = fh.read()
 
-if __name__ == "__main__":
-    setuptools.setup(name="cellphonedb")
+with open("requirements.txt", "rt", encoding="utf-8") as fh:
+    install_requirements_txt = [line.strip() for line in fh.readlines()]
+
+setup(
+    name='CellphoneDB',
+    author='TeichLab/VentoLab',
+    author_email='contact@cellphonedb.org',
+    version=version,
+    description='Inferring cell-cell communication',
+    long_description=long_description_readme_md,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    maintainer='VentoLab',
+    maintainer_email='contact@cellphonedb.org',
+    url='https://cellphonedb.org',
+    license='MIT',
+    exclude_package_data={'': []},
+    entry_points={},
+    install_requires=install_requirements_txt,
+)
