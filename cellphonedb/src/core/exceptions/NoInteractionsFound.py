5886d169c2fd4dbf1affd311a9af278f4d75a499
Robert Petryszak <info@datasome.co.uk>
2023-04-19 15:55:44 +0100

    Fixed typo

--- cellphonedb/src/core/exceptions/NoInteractionsFound.py
+++ cellphonedb/src/core/exceptions/NoInteractionsFound.py
@@ -1,5 +1,5 @@
 class NoInteractionsFound(Exception):
     def __init__(self, description: str = None, hint: str = None):
-        super(NoInteractionsFound, self).__init__('No CellPhoneDB interacions found in this input.')
+        super(NoInteractionsFound, self).__init__('No CellphoneDB interactions found in this input.')
         self.description = description
         self.hint = hint
